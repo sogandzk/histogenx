697570691b6b92f902e85963f33861bbf6de53ef
Sogand Zamani Khah <44114617+sogandzk@users.noreply.github.com>
2026-01-26 11:20:36 -0500

    Update config.py

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,7 +1,1 @@
-# PROJECT_DIR = "/workspace/HistGen-Alignment"
-
-PROJECT_DIR = "/Users/mbayati/Desktop/Sogand/branches/train-method-average/HistGen-Alignment"
-
-# PROJECT_DIR = "/Users/sogand/Desktop/HistGen-Alignment"
-
-
+PROJECT_DIR = "/workspace/HistGen-Alignment"
