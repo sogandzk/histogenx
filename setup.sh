7036dfa4363b86de5ff57e75dc21f2e4635ef14f
sogandzk <sogand.zamanikhah@gmail.com>
2026-05-10 00:48:09 -0400

    update

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ pip install optuna optuna-integration[pytorch_lightning]
 mkdir data
 cd data
 
-gdown --fuzzy https://drive.google.com/file/d/1QMjQzndn7ym5zcuaHzKhJcrdMOxvMhgL/view?usp=sharing
+gdown --fuzzy https://drive.google.com/file/d/1tjyC1WgVbmb6G1q0ilm-Fgq53VilI5R5/view?usp=sharing
 
 tar -xzf dataset.tar.gz
 rm dataset.tar.gz
