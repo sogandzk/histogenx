ccc1e1a3e1086584bbaac25ceccac61cb0a2807f
sogandzk <sogand.zamanikhah@gmail.com>
2026-05-10 15:43:09 +0000

    Update

--- src/train.py
+++ src/train.py
@@ -19,15 +19,15 @@ def objective(trial, dataset):
     hparams = {
         "max_epochs": 100,
         "k_patches": trial.suggest_categorical("k_patches", [4, 8, 16]),
-        "batch_size": trial.suggest_categorical("batch_size", [16, 32, 64]),
-        "learning_rate": trial.suggest_float("learning_rate", 1e-6, 1e-3, log=True),
-        "weight_decay": trial.suggest_float("weight_decay", 1e-6, 1e-3, log=True),
-        "temperature": trial.suggest_float("temperature", 0.05, 0.3),
-        "dropout1": 0.2, #trial.suggest_float("dropout1", 0, 0.6), 
+        "batch_size": trial.suggest_categorical("batch_size", [32, 64, 128, 256]),
+        "learning_rate": trial.suggest_float("learning_rate", 1e-6, 1e-4, log=True),
+        "weight_decay": trial.suggest_float("weight_decay", 1e-6, 1e-4, log=True),
+        "temperature": trial.suggest_float("temperature", 0.01, 0.3),
+        "dropout1": 0.2, #trial.suggest_float("dropout1", 0, 0.6),
         "dropout2": 0.3, #trial.suggest_float("dropout2", 0, 0.6),
         # "img_depth": trial.suggest_categorical("img_depth", ["shallow", "deep"]),
     }
-    hparams["img_hidden_dims"] = [512, 256]# if hparams["img_depth"] == "deep" else [512]
+    hparams["img_hidden_dims"] = [512, 256]
     hparams["expr_hidden_dims"] = [512, 256]
     hparams["img_dropout_probs"] = [hparams["dropout1"]] + [hparams["dropout2"]] * (len(hparams["img_hidden_dims"]) - 1)
     hparams["expr_dropout_probs"] = [hparams["dropout1"]] + [hparams["dropout2"]] * (len(hparams["expr_hidden_dims"]) - 1)
@@ -94,14 +94,14 @@ def main():
     for tag in ['A', 'B', 'C', 'D']:
         seed = ord(tag)
         study = optuna.create_study(
-            study_name=f"May_9_2025_{tag}",
+            study_name=f"May_10_2025_{tag}",
             direction="minimize",
-            sampler=optuna.samplers.TPESampler(n_startup_trials=15, seed=seed),
-            pruner=optuna.pruners.MedianPruner(n_startup_trials=10, n_warmup_steps=20, interval_steps=5),
+            sampler=optuna.samplers.TPESampler(n_startup_trials=20, seed=seed),
+            pruner=optuna.pruners.MedianPruner(n_startup_trials=40, n_warmup_steps=20, interval_steps=5),
             storage=f"sqlite:///{PROJECT_DIR}/experiments/optuna.db",
             load_if_exists=True
         )
-        study.optimize(lambda trial: objective(trial, dataset), n_trials=60)
+        study.optimize(lambda trial: objective(trial, dataset), n_trials=100)
 
     print("*** DONE ***")
 
