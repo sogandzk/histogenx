572de9a7579bf01bff622af9678d96dab993299a
sogandzk <sogand.zamanikhah@gmail.com>
2026-05-12 22:24:21 +0000

    Update

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,5 +1,5 @@
 # PROJECT_DIR = "/workspace/HistGen-Alignment"
 
-# PROJECT_DIR = "/root/HistoGenX"
-PROJECT_DIR = "/Users/mbayati/Desktop/HistoGenX"
+PROJECT_DIR = "/root/HistoGenX"
+#PROJECT_DIR = "/Users/mbayati/Desktop/HistoGenX"
 
